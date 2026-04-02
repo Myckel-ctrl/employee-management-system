d20468cb1e2606206458c8f9ca81e6c76bd23a68
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-04-02 16:20:55 -0300

    feat: adiciona construtor na classe Funcionario

--- system/src/model/Funcionario.java
+++ system/src/model/Funcionario.java
@@ -7,5 +7,12 @@ public class Funcionario {
     private String telefone;
     private String setor;
 
+    public Funcionario(String nome, String cpf, String telefone, String setor) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.setor = setor;
+    }
+
 
 }
