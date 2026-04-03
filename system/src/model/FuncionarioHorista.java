ae177f169085de794a1e6764544dcfe8754d9451
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-04-02 21:58:25 -0300

    feat: implementa construtor para inicializar dados do FuncionarioHorista

--- system/src/model/FuncionarioHorista.java
+++ system/src/model/FuncionarioHorista.java
@@ -5,5 +5,9 @@ public class FuncionarioHorista extends Funcionario{
     private int hora;
     private double valorHora;
 
-
+    public FuncionarioHorista(String nome, String cpf, String telefone, String setor, int hora, double valorHora) {
+        super(nome, cpf, telefone, setor);
+        this.hora = hora;
+        this.valorHora = valorHora;
+    }
 }
