5e3eec018f1c95f07ba665d2256fb6ca582412b9
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-04-02 22:29:32 -0300

    feat: adiciona cadastro de funcionarios assalariados e horistas com validações

--- system/src/service/FuncionarioService.java
+++ system/src/service/FuncionarioService.java
@@ -1,5 +1,10 @@
 package service;
 
+import exception.HoraOuValorInvalidoException;
+import exception.SalarioInvalidoException;
+import model.Funcionario;
+import model.FuncionarioAssalariado;
+import model.FuncionarioHorista;
 import repository.FuncionarioRepository;
 
 public class FuncionarioService {
@@ -8,5 +13,23 @@ public class FuncionarioService {
 
     public FuncionarioService(FuncionarioRepository repository) {
         this.repository = repository;
+    }
+
+    public void cadastrarHorista(String nome, String cpf, String telefone, String setor, int hora, double valorHora){
+        if (hora <= 0 || valorHora <= 0) {
+            throw new HoraOuValorInvalidoException("Hora ou valor inválido.");
+        }
+
+        Funcionario funcionario = new FuncionarioHorista(nome, cpf, telefone, setor, hora, valorHora);
+        repository.salvar(funcionario);
+    }
+
+    public void cadastrarAssalariado(String nome, String cpf, String telefone, String setor, double salario){
+        if (salario <= 0) {
+            throw new SalarioInvalidoException("Salário inválido.");
+        }
+
+        Funcionario funcionario = new FuncionarioAssalariado(nome, cpf, telefone, setor, salario);
+        repository.salvar(funcionario);
     }
 }
