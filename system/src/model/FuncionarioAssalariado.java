a3696ae2e3120b909b211ac7c53b008b08660340
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-04-02 21:52:19 -0300

    feat: adiciona atributo salario para FuncionarioAssalariado

--- system/src/model/FuncionarioAssalariado.java
+++ system/src/model/FuncionarioAssalariado.java
@@ -2,4 +2,10 @@ package model;
 
 public class FuncionarioAssalariado extends Funcionario{
 
+    private double salario;
+
+    public FuncionarioAssalariado(String nome, String cpf, String telefone, String setor, double salario) {
+        super(nome, cpf, telefone, setor);
+        this.salario = salario;
+    }
 }
