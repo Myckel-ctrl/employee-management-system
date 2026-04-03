b97de1c2ed7f6735db2aa0158f010e36e9554122
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-04-02 22:04:45 -0300

    feat: adiciona método para salvar funcionario no repository

--- system/src/repository/FuncionarioRepository.java
+++ system/src/repository/FuncionarioRepository.java
@@ -3,4 +3,8 @@ package repository;
 public class FuncionarioRepository {
 
     private List<Funcionario> funcionarios = new ArrayList<>();
+
+    public void salvar(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
 }
