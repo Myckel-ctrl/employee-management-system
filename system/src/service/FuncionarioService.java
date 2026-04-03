c57b67c74a50c0c277ef099d77d71d9dd35977c5
Myckel de Vasconcelos Mota <myckel.vasconcelos638@gmail.com>
2026-04-02 22:28:08 -0300

    feat: adiciona repository no service

--- system/src/service/FuncionarioService.java
+++ system/src/service/FuncionarioService.java
@@ -5,4 +5,8 @@ import repository.FuncionarioRepository;
 public class FuncionarioService {
 
     private FuncionarioRepository repository;
+
+    public FuncionarioService(FuncionarioRepository repository) {
+        this.repository = repository;
+    }
 }
